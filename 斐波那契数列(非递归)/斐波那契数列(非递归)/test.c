a941826b20bc21738d83f0d42527e459703a2131
jihaojiemo <914064689@qq.com>
2018-11-11 13:41:54 +0800

    修改版本

--- 斐波那契数列(非递归)/斐波那契数列(非递归)/test.c
+++ 斐波那契数列(非递归)/斐波那契数列(非递归)/test.c
@@ -1,22 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+int Fib(int n) {
+	if (n == 1 || n == 2) {
+		return 1;
+	}
+	// 要求第 n 项，就必须知道 n-1 和 n-2 项
+	// n_1：n-1 项  n_2：n-2 项  n_i：第 i 项
+	int n_1 = 1;
+	int n_2 = 1;
+	// 初始化为 0
+	int n_i = 0;
+	for (int i = 3; i <= n; ++i) {
+		n_i = n_1 + n_2;
+		n_2 = n_1;
+		n_1 = n_i;
+	}
+	return n_i;
+
+}
 int main() {
 	int n;
 	scanf("%d", &n);
-	// i: 次数  j: 第一个数  k: 第二个数  m: 第三个数
-	int j = 1; int k = 1; int m;
-	for (int i = 1; i <= n; i++) {
-		if (i == 1 || i == 2) {
-			m = 1;
-		}
-		else {
-			m = j + k;
-			j = k;
-			k = m;
-		}
-		printf("%d\t", m);
-	}
+	printf("%d ", Fib(n));
 	system("pause");
 	return 0;
 }
