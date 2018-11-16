0e13fe0c64aeaa6f2a606393827c17b83f4d792d
jihaojiemo <914064689@qq.com>
2018-11-16 18:48:26 +0800

    ninecount

---
+++ 1-100中出现9的次数（2）/1-100中出现9的次数（2）/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int NineCount(int x) {
+	int count = 0;
+	// 两位数
+	// 判断个位
+	if (x % 10 == 9) {
+		++count;
+	}
+	// 判断十位
+	if (x / 10 == 9) {
+		++count;
+	}
+	return count;
+}
+int main() {
+	int count = 0;
+	for (int i = 1; i < 100; i++) {
+		count += NineCount(i);
+	}
+	printf("%d\n", count);
+	system("pause");
+	return 0;
+}
