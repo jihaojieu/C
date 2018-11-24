4a851d7397f1117534a3666eb3d11dde04dfcd0c
jihaojiemo <914064689@qq.com>
2018-11-24 17:46:00 +0800

    按顺序输出

---
+++ 输出一个整数的每一位/输出一个整数的每一位/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int Factor(int num) {
+	if (num > 9) {
+		Factor(num / 10);
+	}
+	printf("%d\n",num % 10);
+}
+int main() {
+	int num = 0;
+	scanf("%d", &num);
+	Factor(num);
+	system("pause");
+	return 0;
+}
