35778229a200bd544a7749407dda1c13cfa07b1e
jihaojiemo <914064689@qq.com>
2018-11-24 17:57:04 +0800

    ~

--- 输出一个整数的每一位/输出一个整数的每一位/test.c
+++ 输出一个整数的每一位/输出一个整数的每一位/test.c
@@ -5,7 +5,52 @@ int Factor(int num) {
 	if (num > 9) {
 		Factor(num / 10);
 	}
-	printf("%d\n",num % 10);
+	printf("%d\n", num % 10);
+}
+int main() {
+	int num = 0;
+	scanf("%d", &num);
+	Factor(num);
+	system("pause");
+	return 0;
+}#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int Factor(int num) {
+	if (num > 9) {
+		Factor(num / 10);
+	}
+	printf("%d\n", num % 10);
+}
+int main() {
+	int num = 0;
+	scanf("%d", &num);
+	Factor(num);
+	system("pause");
+	return 0;
+}#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int Factor(int num) {
+	if (num > 9) {
+		Factor(num / 10);
+	}
+	printf("%d\n", num % 10);
+}
+int main() {
+	int num = 0;
+	scanf("%d", &num);
+	Factor(num);
+	system("pause");
+	return 0;
+}#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int Factor(int num) {
+	if (num > 9) {
+		Factor(num / 10);
+	}
+	printf("%d\n", num % 10);
 }
 int main() {
 	int num = 0;
