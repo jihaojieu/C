438055fc19c74b0fcf5f3fc1754d40f05d2d525e
jihaojiemo <914064689@qq.com>
2018-11-26 13:34:43 +0800

    ~

--- 一个整数中二进制1的个数/一个整数中二进制1的个数/test.c
+++ 一个整数中二进制1的个数/一个整数中二进制1的个数/test.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 // 仅限于正数，因为负数 % 2 是 -1
-int BitOneCount(int num) {
-	int count = 0;
-	while (num) {
-		if (num % 2 == 1) {
-			++count;
-		}
-		num /= 2;
-	}
-	return count;
-}
+//int BitOneCount(int num) {
+//	int count = 0;
+//	while (num) {
+//		if (num % 2 == 1) {
+//			++count;
+//		}
+//		num /= 2;
+//	}
+//	return count;
+//}
 // 确保每一位都进行过比较，第 i 位是否为 1
 int BitOneCount(int num) {
 	int count = 0;
