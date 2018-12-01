e3e3f1209af97b10a37e029fdc2ae5e679aa0d48
jihaojiemo <44723040+jihaojiemo@users.noreply.github.com>
2018-12-01 16:57:06 +0800

    Update test.c

--- 求一个整数中二进制1的个数/求一个整数中二进制1的个数/test.c
+++ 求一个整数中二进制1的个数/求一个整数中二进制1的个数/test.c
@@ -1,29 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-// 仅限于正数，因为负数 % 2 是 -1
-//int BitOneCount(int num) {
-//	int count = 0;
-//	while (num) {
-//		if (num % 2 == 1) {
-//			++count;
-//		}
-//		num /= 2;
-//	}
-//	return count;
-//}
-<<<<<<< HEAD
-// ȷ��ÿһλ�����й��Ƚϣ��� i λ�Ƿ�Ϊ 1
-int BitOneCount_1(int num) {
-=======
-
-// 和1进行&操作可图得到该位上的二进制值
 // 确保每一位都进行过比较，第 i 位是否为 1
-int BitOneCount(int num) {
->>>>>>> 9b3114b998d9dd3b3f7496771f2f4addb0503b9c
+int BitOneCount_1(int num) {
 	int count = 0;
 	for (int i = 0; i < 32; ++i) {
-		// ��һλ����0λ
+		// 第一位右移0位
 		if ((num >> i) & 1 ) {
 			++count;
 		}
@@ -34,7 +16,7 @@ int BitOneCount_2(int num) {
 	int count = 0;
 	while (num) {
 		++count;
-		// �ӵ�λ����λ���ѵ�һ�γ��� 1 ��λ�ñ�� 0
+		// 从低位到高位，把第一次出现 1 的位置变成 0
 		num = num & (num - 1);
 	}
 	return count;
