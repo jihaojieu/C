4cab6907bc3dfef9571c6019f836f84d92a814a1
jihaojiemo <914064689@qq.com>
2018-12-01 16:51:16 +0800

    二进制1的个数

--- 求一个整数中二进制1的个数/求一个整数中二进制1的个数/test.c
+++ 求一个整数中二进制1的个数/求一个整数中二进制1的个数/test.c
@@ -13,19 +13,30 @@
 //	return count;
 //}
 // 确保每一位都进行过比较，第 i 位是否为 1
-int BitOneCount(int num) {
+int BitOneCount_1(int num) {
 	int count = 0;
-	for (int i = 1; i <= 32; ++i) {
-		if (num & (1 << i)) {
+	for (int i = 0; i < 32; ++i) {
+		// 第一位右移0位
+		if ((num >> i) & 1 ) {
 			++count;
 		}
+	}
+	return count;
+}
+int BitOneCount_2(int num) {
+	int count = 0;
+	while (num) {
+		++count;
+		// 从低位到高位，把第一次出现 1 的位置变成 0
+		num = num & (num - 1);
 	}
 	return count;
 }
 int main() {
 	int num = 0;
 	scanf("%d", &num);
-	printf("%d\n", BitOneCount(num));
+	printf("%d\n", BitOneCount_1(num));
+	printf("%d\n", BitOneCount_2(num));
 	system("pause");
 	return 0;
 }
